d2a5cffe66ae18d13ecc5d73b427e1363f104ca6
fraespre <fraespre@gmail.com>
2018-09-11 22:36:00 +0000

    changes

--- script/cloud9-java.sh
+++ script/cloud9-java.sh
@@ -1,9 +1,3 @@
-# sudo apt-get remove maven2 -y
-# sudo apt-get install maven -y
-# sudo add-apt-repository ppa:webupd8team/java -y
-# sudo apt-get update
-# sudo apt-get install oracle-java8-installer
-
 
 # Install Java 1.8
 sudo echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
@@ -16,3 +10,8 @@ sudo rm -rf /var/cache/oracle-jdk8-installer
 # Define commonly used JAVA_HOME variable and update PATH
 echo 'export JAVA_HOME=/usr/lib/jvm/java-8-oracle' >> ~/.bashrc
 echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.bashrc 
+java -version
+
+# Install Maven2
+sudo apt-get remove maven2 -y
+sudo apt-get install maven -y
